5cc6f3ac2b4f586dafbfeb835cf57973cb8d503d
cindy-harvard <62086685+cindy-harvard@users.noreply.github.com>
2020-05-17 14:54:09 -0700

    installed sass and made button component

--- hello-world/gatsby-config.js
+++ hello-world/gatsby-config.js
@@ -5,6 +5,11 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    // ...
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    // ...
+  ],
 }
--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-import Header from "../components/header"
+import "../styles/styles.scss"
+import Button from "../components/button"
 
 export default function Home() {
   return (
@@ -8,6 +9,9 @@ export default function Home() {
       <Link to="/contact/">Contact</Link>
       <h1>Hello Gatsby!</h1>
       <p>What a world.</p>
+      <p>
+        <Button buttonText="Click me" />
+      </p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
